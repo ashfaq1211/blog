f0f8d9705e6a9e68bb1cbf95e69ba74a94f3b3df
ashfaq1211 <34036382+ashfaq1211@users.noreply.github.com>
2021-02-25 14:06:21 +0600

    Initial commit.

---
+++ app.js
@@ -0,0 +1,104 @@
+//jshint esversion:6
+
+// REQUIRING INSTALLED MODULES
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+
+// SETTING UP THE MODULES TO BE USED
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+// HOME ROUTE
+
+app.get("/", function(req, res) {
+  res.render("home", {
+    para:homeStartingContent,
+    postsArr: posts
+  });
+
+});
+
+
+// ABOUT ROUTE
+
+app.get("/about", function(req, res) {
+  res.render("about", {about:aboutContent});
+});
+
+
+// CONTACT ROUTE
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {contact:contactContent});
+});
+
+// COMPOSE ROUTE
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res) {
+
+  var postData = {
+    titleText: req.body.titleText,
+    postText: req.body.postText
+  };
+
+  posts.push(postData);
+
+  res.redirect("/");
+
+});
+
+
+// EJS PARAMETERS
+
+// app.get("/posts/:blog", function(req, res) {
+//   console.log(req.params.blog);
+// });
+
+app.get("/posts/:title", function(req, res) {
+
+  const reqTitle = _.lowerCase(req.params.title);
+
+  posts.forEach(function(i){
+
+    const storedTitle =  _.lowerCase((i.titleText));
+
+    if(reqTitle === storedTitle){
+
+      res.render("post", {
+        postTitle: i.titleText,
+        postBody: i.postText
+      });
+
+    } else {
+      res.redirect("/");
+    }
+
+  });
+
+});
+
+
+// RUNNING APP ON LOCAL SERVER
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
